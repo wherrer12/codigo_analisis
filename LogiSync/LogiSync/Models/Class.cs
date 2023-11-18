00defc40dfb209aff146b3b7d6ceee86d8ff3ff4
Keyner10 <keynerchavarria1924@gmail.com>
2023-11-18 14:57:20 -0600

    Cambios en class.cs

--- LogiSync/LogiSync/Models/Class.cs
+++ LogiSync/LogiSync/Models/Class.cs
@@ -6,7 +6,8 @@
         int num23 = 2;
         string caso = "Funciona";
         int test = 9;
-        string = "prueba Vir";
-            string = "prueba andres";
+        string hola = "prueba Vir";
+            string hola1 = "prueba andres";
+        string prueba = "Prueba";
     }
 }
