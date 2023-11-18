6aae64ae11823bb7dc70fec038784894b8201380
wherrer12 <86135942+wherrer12@users.noreply.github.com>
2023-11-18 15:29:02 -0600

    Borrado

--- LogiSync/LogiSync/Models/Class.cs
+++ LogiSync/LogiSync/Models/Class.cs
@@ -2,16 +2,6 @@
 {
     public class Class
     {
-        int num = 0;
-        int num23 = 2;
-        string caso = "Funciona";
-        int test = 9;
-        string hola = "prueba Vir";
-            string hola1 = "prueba andres";
-        string prueba = "Prueba";
-        bool m = true;
-        string NOMBRE ="jean";
-        string prueba2 = "";
-        string sirve = "si";
+        string borrado = "si";
     }
 }
