29ba75c18b32341dca0c2ba0ff30a0fe00cfd5ee
wherrer12 <86135942+wherrer12@users.noreply.github.com>
2023-11-20 17:07:35 -0600

    INT L

--- LogiSync/LogiSync/Controllers/Class.cs
+++ LogiSync/LogiSync/Controllers/Class.cs
@@ -4,19 +4,6 @@
     {
 
         int t = 0;
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
         int l = 0;
-        int r = 0;
-        int i = 0;
-        int j = 0;
-=======
->>>>>>> Clase ficticia
-=======
-        int l = 0;
->>>>>>> INT L
-=======
->>>>>>> Clase ficticia
     }
 }
